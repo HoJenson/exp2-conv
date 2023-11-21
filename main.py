4a039db4eb97698c7ba47499b4211484bb024aad
HoJenson <hojeson@163.com>
2023-11-21 14:14:01 +0800

    训练dropout

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from train_model import Lab2Model
 
 if __name__ == '__main__':
-    model = Lab2Model(batch_size=64, num_workers=8, seed=0)
-
     # 待调试的超参数
     dropout = True
     ps = [0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9]
@@ -13,11 +11,13 @@ if __name__ == '__main__':
 
     # 最好还是使用 GPU, CPU 太慢了。。。
     # 选择dropout参数
-    model.train(lr=0.001, epochs=80, device='cuda', wait=4, lrd=lrd, fig_name='Final',
+    model = Lab2Model(batch_size=128, num_workers=4, seed=0)
+    model.train(lr=0.001, epochs=80, device='cuda', wait=4, lrd=lrd, fig_name='without_dropout',
                 p=0, channels=channels, residual_connection=residual_connection, 
                 normalize=normalize, dropout=False)
     for p in ps:
-        model.train(lr=0.001, epochs=80, device='cuda', wait=4, lrd=lrd, fig_name=f"{int(p*10)}_dropout",
+        model = Lab2Model(batch_size=128, num_workers=4, seed=0)
+        model.train(lr=0.001, epochs=80, device='cuda', wait=4, lrd=lrd, fig_name=f"{int(p*10)}__dropout",
                     p=p, channels=channels, residual_connection=residual_connection, 
                     normalize=normalize, dropout=dropout)
 
